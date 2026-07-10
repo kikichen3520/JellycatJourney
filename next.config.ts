afeb4a39be6669bd53be02305ac88420833da788
Kiki Chen <kikichen@Kikis-MacBook-Pro.local>
2026-07-10 15:08:20 -0700

    update next auth path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
+    // Trace the custom Prisma folder for all your backend API routes
+    "/api/**/*": ["./app/generated/prisma/**/*"],
+    // Trace it for standard app/pages routing entry points
     "/**/*": ["./app/generated/prisma/**/*"],
   },
 };
